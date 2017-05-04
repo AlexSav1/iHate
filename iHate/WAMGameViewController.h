c7625c2d99900a7b964781ba7983172c29dec72f
Alex Laptop <savittalex@gmail.com>
2017-05-04 19:37:52 -0400

    added some inital gameVC logic

--- iHate/WAMGameViewController.h
+++ iHate/WAMGameViewController.h
@@ -10,6 +10,26 @@
 
 @interface WAMGameViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIImageView *imageView6;
+@property int howOftenHeadsPopUp;
+
+@property int numberOfHeadsPoppingUp;
+
+//@property NSArray
+
+@property UIImage *currentVictim;
+
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+@property (weak, nonatomic) IBOutlet UIButton *button9;
+@property (weak, nonatomic) IBOutlet UIButton *button10;
+@property (weak, nonatomic) IBOutlet UIButton *button11;
+@property (weak, nonatomic) IBOutlet UIButton *button12;
+
 
 @end
