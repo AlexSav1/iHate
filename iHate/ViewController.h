834faf4a49e2b60a314b579c8fef377f3f0dd412
Alex Laptop <savittalex@gmail.com>
2017-05-04 16:46:47 -0400

    init commit

---
+++ iHate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iHate
+//
+//  Created by Alex Laptop on 5/4/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iHate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iHate
+//
+//  Created by Alex Laptop on 5/4/17.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
