f90feb62a3a66006e90c6ed31b63b8c9bb552ad3
Aditya Narayan <savittalex@gmail.com>
2017-05-05 13:22:24 -0400

    finished dao

--- iHate/DAO.h
+++ iHate/DAO.h
@@ -7,17 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PlayerScore.h"
 @import Firebase;
 
 @interface DAO : NSObject
 @property (strong, nonatomic) NSArray *games;
 //@property (strong, nonatomic) NSMutableArray *topScores;
 @property NSDictionary *scoresDict;
-@property NSMutableArray *topNames;
-@property NSMutableArray *topScores;
+@property NSMutableArray *playerScores;
 @property int currentScore;
 @property (strong, nonatomic) FIRDatabaseReference *ref;
 
 + (id)sharedDataManager;
 -(void) fetchFromDatabase;
+-(void) patchDataBase: (NSNumber*) newScore
+              andName: (NSString*) newName;
 @end
