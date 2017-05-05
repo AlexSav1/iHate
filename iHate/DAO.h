720f23d64a2524d909495628e0bbf1361aa32ac6
Avery <imthatninjatho@gmail.com>
2017-05-05 16:44:45 -0400

    added bgMusic method to DAO

--- iHate/DAO.h
+++ iHate/DAO.h
@@ -8,9 +8,17 @@
 
 #import <Foundation/Foundation.h>
 #import "PlayerScore.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
 @import Firebase;
 
 @interface DAO : NSObject
+@property(nonatomic, strong) AVAudioPlayer *backgroundMusic;
+@property NSString *currenBGSong;
+
+@property (strong, nonatomic) UIImage *currentVictim;
+
+
 @property (strong, nonatomic) NSArray *games;
 //@property (strong, nonatomic) NSMutableArray *topScores;
 @property NSDictionary *scoresDict;
@@ -20,6 +28,7 @@
 @property (strong, nonatomic) FIRDatabaseReference *ref;
 
 + (id)sharedDataManager;
+-(void) bgMusic;
 -(void) fetchFromDatabase;
 -(void) patchDataBase: (NSNumber*) newScore
               andName: (NSString*) newName;
